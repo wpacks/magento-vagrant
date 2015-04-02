dbbf824fa78a8c846af9386a8422a8184ef7848f
Sang Truong <thanhsangvnm@gmail.com>
2015-04-02 21:37:12 +0700

    Installed php5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install php5-fpm
+
+apt-get install -y php5-fpm git
+rm /etc/php5/fpm/php.ini
+cp /vagrant/php/php.ini /etc/php5/fpm/php.ini
+service php5-fpm restart
